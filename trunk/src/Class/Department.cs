f090e0a79a4c8175139adcaa3f0a3219650ab07f
minhmoron <minhmoron@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-01-12 12:45:54 +0000

    add data access and object classes

---
+++ trunk/src/Class/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIDAppVBEta.Class
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CompanyID { get; set; }
+        public int SupDepartmentID { get; set; }
+    }
+}
