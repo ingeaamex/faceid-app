b2c38ad65f62de6fd88a78085562bcef7894355e
sunflj2me <sunflj2me@gmail.com>
2010-01-12 07:03:39 +0000

    update

---
+++ src/Business/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace FaceIDpp.Business
+{
+    public class Company : BaseData
+    {
+        public Company()
+        { 
+            
+        }
+
+        public DataTable GetData()
+        {
+            //objData.connection.Open();
+            DataTable dt = objData.GetDataTable("select * from Company");
+            //objData.connection.Close();
+            return dt;
+        }
+    }
+}
