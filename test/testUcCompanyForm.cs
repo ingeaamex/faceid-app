2b0c37016909c35e3d53e279b07fd31d5db96882
minhmoron <minhmoron@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-01-12 12:40:49 +0000

    add unit tests

---
+++ test/testUcCompanyForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using FaceIDAppVBEta.Data;
+using FaceIDAppVBEta.Class;
+
+namespace FaceIDAppVBEta.UnitTest
+{
+    [TestFixture]
+    class testUcCompanyForm
+    {
+        [Test]
+        public void AddCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            string companyName = "ABC";
+            Company company = new Company();
+            company.Name = companyName;
+
+            Assert.Greater(dtCtrl.AddCompany(company), 0);
+            dtCtrl.DeleteCompany(company);
+        }
+
+        [Test]
+        public void AddEmptyNamedCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            string companyName = "";
+            Company company = new Company();
+            company.Name = companyName;
+
+            Assert.Greater(dtCtrl.AddCompany(company), 0);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void AddDuplicatedCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            string companyName = "ABC";
+            Company company = new Company();
+            company.Name = companyName;
+
+            dtCtrl.AddCompany(company);
+            dtCtrl.AddCompany(company);
+        }
+
+        [Test]
+        public void UpdateCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            Company company1 = new Company();
+            company1.Name = "ABC";
+
+            dtCtrl.AddCompany(company1);
+
+            company1.Name = "123";
+
+            Assert.AreEqual(dtCtrl.UpdateCompany(company1), true);
+        }
+
+        [Test]
+        public void UpdateEmptyNamedCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            string companyName = "";
+            Company company = new Company();
+            company.Name = companyName;
+
+            Assert.AreEqual(dtCtrl.UpdateCompany(company), false);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void UpdateDuplicatedCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            string companyName = "ABC";
+            
+            Company company1 = new Company();
+            company1.Name = companyName;
+
+            Company company2 = new Company();
+            company2.Name = companyName;
+
+            dtCtrl.UpdateCompany(company1);
+            dtCtrl.UpdateCompany(company2);
+        }
+
+        [Test]
+        public void DeleteCompanyTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            string companyName = "ABC";
+
+            Company company1 = new Company();
+            company1.Name = companyName;
+
+            dtCtrl.AddCompany(company1);
+            Assert.AreEqual(dtCtrl.DeleteCompany(company1), true);
+        }
+
+        [Test]
+        public void DeleteNotEmptiedCompanyTest()
+        {
+            //to be added later
+        }
+    }
+}
---
+++ test/testUcDepartmentForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using FaceIDAppVBEta.Data;
+using FaceIDAppVBEta.Class;
+
+namespace FaceIDAppVBEta.UnitTest
+{
+    [TestFixture]
+    class testUcDepartmentForm
+    {
+        [Test]
+        public void AddUpdateDeleteDepartmentTest()
+        {
+            IDataController dtCtrl = LocalDataController.Instance;
+
+            //Add a new department
+            Department dept1 = new Department();
+            dept1.Name = "Dept1";
+            dept1.CompanyID = 1;
+
+            dept1.ID = dtCtrl.AddDepartment(dept1);
+            Assert.Greater(dept1.ID, 0);
+
+            //Add a new sub-department
+            Department dept2 = new Department();
+            dept2.Name = "Dept2";
+            dept2.CompanyID = 1;
+            dept2.SupDepartmentID = dept1.ID;
+
+            dept2.ID = dtCtrl.AddDepartment(dept2);
+            Assert.Greater(dept2.ID, 0);
+
+            //Add a empty named department
+            Department dept3 = new Department();
+            dept3.Name = "";
+            dept3.CompanyID = 1;
+
+            Assert.Less(dtCtrl.AddDepartment(dept3), 0);
+
+            //Add a duplicated department
+            Department dept4 = new Department();
+            dept4.Name = "Dept1";
+            dept4.CompanyID = 1;
+
+            Assert.Less(dtCtrl.AddDepartment(dept4), 0);
+
+            //Add a non-company department
+            Department dept5 = new Department();
+            dept5.Name = "Dept5";
+
+            Assert.Less(dtCtrl.AddDepartment(dept5), 0);
+
+            //Update an existing department
+            Assert.AreEqual(dtCtrl.UpdateDepartment(dept1), true);
+
+            //Update an exist department using an exist Name
+            dept1.Name = "Dept2";
+            Assert.AreEqual(dtCtrl.UpdateDepartment(dept1), false);
+
+            //Update an exist department using an empty Name
+            dept1.Name = "";
+            Assert.AreEqual(dtCtrl.UpdateDepartment(dept1), false);
+
+            //Delete exist departments
+            Assert.AreEqual(dtCtrl.DeleteDepartment(dept1), true);
+            Assert.AreEqual(dtCtrl.DeleteDepartment(dept2), true);
+        }
+    }
+}
