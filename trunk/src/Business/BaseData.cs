ec7e9deef262f96f92bc71d12a441081809d2189
sunflj2me@gmail.com <sunflj2me@gmail.com@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-01-12 07:03:39 +0000

    update

---
+++ trunk/src/Business/BaseData.cs
@@ -0,0 +1,12 @@
+﻿namespace FaceIDpp.Business
+{
+    public class BaseData
+    {
+        protected DBUtils.DatabaseAccess objData;
+        public BaseData()
+        {
+            objData = new DBUtils.DatabaseAccess();
+            objData.CreateConnection(@"F:\vnanh\project\FaceID\db\FaceIDdb.mdb");
+        }
+    }
+}
