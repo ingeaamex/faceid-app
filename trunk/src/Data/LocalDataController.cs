1c8238430291df2f527d1a05e1806ffee4e9ae3c
minhmoron <minhmoron@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-01-23 06:13:31 +0000

    fix some bugs

--- trunk/src/Data/LocalDataController.cs
+++ trunk/src/Data/LocalDataController.cs
@@ -1254,10 +1254,10 @@ namespace FaceIDAppVBEta.Data
 
         public bool UpdateWorkingCalendar(WorkingCalendar workingCalendar, List<Break> breakList, List<Holiday> holidayList, PaymentRate workingDayPaymentRate, PaymentRate nonWorkingDayPaymentRate, PaymentRate holidayPaymentRate, PayPeriod payPeriod)
         {
-            BeginTransaction();
+            //BeginTransaction();
 
-            try
-            {
+            //try
+            //{
                 //update pay period
                 PayPeriod oldPayPeriod = GetPayPeriod(workingCalendar.PayPeriodID);
 
@@ -1328,13 +1328,13 @@ namespace FaceIDAppVBEta.Data
                 if (AddPaymentRate(holidayPaymentRate) < 0)
                     throw new NullReferenceException();
 
-                CommitTransaction();
-            }
-            catch (Exception)
-            {
-                RollbackTransaction();
-                return false;
-            }
+                //CommitTransaction();
+            //}
+            //catch (Exception)
+            //{
+            //    RollbackTransaction();
+            //    return false;
+            //}
 
             return true;
         }
@@ -2150,7 +2150,8 @@ namespace FaceIDAppVBEta.Data
 
         public bool DeletePayPeriod(int id)
         {
-            throw new NotImplementedException();
+            System.Data.OleDb.OleDbCommand odCom1 = BuildDelCmd("PayPeriod", "ID=@ID", new object[] { "@ID", id });
+            return odCom1.ExecuteNonQuery() > 0 ? true : false;
         }
 
         public int AddPayPeriodType(PayPeriodType payPeriodType)
