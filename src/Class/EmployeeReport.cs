6eff1ad6270701abed1ea4f5fdf6ee46ebf8cd88
sunflj2me <sunflj2me@gmail.com>
2010-01-25 07:26:17 +0000

    add

---
+++ src/Class/EmployeeReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIDAppVBEta.Class
+{
+    public class EmployeeReport
+    {
+        public int EmployeeNo { get; set; }
+        public string DepartmentName { get; set; }
+        public string FullName { get; set; }
+        public string JobDescription { get; set; }
+        public DateTime HiredDate { get; set; }
+    }
+}
