82127c760add450faa9ce477cacb2f917ad95c19
minhmoron <minhmoron@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-01-25 07:41:54 +0000

    add Rate class

---
+++ src/Class/Rate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIDAppVBEta.Class
+{
+    public class Rate
+    {
+        public Rate(double value, string name)
+        {
+            this.Value = value;
+            this.Name = name;
+        }
+
+        public double Value { get; set; }
+        public String Name { get; set; }
+    }
+}
