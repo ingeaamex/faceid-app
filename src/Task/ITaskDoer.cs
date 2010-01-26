6fe4c3103bddf608e65ba79684c126f7dd356d29
minhmoron <minhmoron@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-01-26 03:46:41 +0000

    add TaskDoer

---
+++ src/Task/ITaskDoer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIDAppVBEta.Task
+{
+    public interface ITaskDoer
+    {
+        bool RemoveEmployeeFromTerminal();
+
+        bool CalculateAttendanceRecord();
+    }
+}
---
+++ src/Task/TaskDoer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIDAppVBEta.Task
+{
+    public class TaskDoer : ITaskDoer
+    {
+        #region ITaskDoer Members
+
+        public bool RemoveEmployeeFromTerminal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CalculateAttendanceRecord()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
