232c3cf9940d19ee9b7799e7e5661e3f9d46ae51
sunflj2me <sunflj2me@gmail.com>
2010-01-27 10:47:46 +0000

    add PayrollExport

---
+++ src/Class/PayrollExport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIDAppVBEta.Class
+{
+    public class PayrollExport
+    {
+        public int PayrollNumber { get; set; }
+        public int EmployeeNumber { get; set; }
+        public string FullName { get; set; }
+        public string JobDescription { get; set; }
+        public double RegularHour { get; set; }
+        public string OvertimeHour { get; set; }
+        public double TotalHours { get; set; }
+        public double TotalHoursWithRate { get; set; }
+        public double TotalOvertimeHours { get; set; }
+    }
+}
