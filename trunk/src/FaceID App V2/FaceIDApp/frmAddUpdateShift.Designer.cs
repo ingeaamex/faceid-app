413fda99da16b2df4b9baae80875b77b389ae6a0
minhmoron <minhmoron@b9e74976-fb40-11de-8745-6fbcd7b84749>
2010-03-02 06:58:01 +0000

    add frmAddUpdateShift

---
+++ trunk/src/FaceID App V2/FaceIDApp/frmAddUpdateShift.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace FaceIDAppVBEta
+{
+    partial class frmAddUpdateShift
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddUpdateShift));
+            this.lblAddUpdateShift = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.lblNextDay = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAddUpdateShift
+            // 
+            this.lblAddUpdateShift.AutoSize = true;
+            this.lblAddUpdateShift.Location = new System.Drawing.Point(102, 13);
+            this.lblAddUpdateShift.Name = "lblAddUpdateShift";
+            this.lblAddUpdateShift.Size = new System.Drawing.Size(88, 13);
+            this.lblAddUpdateShift.TabIndex = 0;
+            this.lblAddUpdateShift.Text = "Add Update Shift";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(68, 169);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Add Update";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(149, 169);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.CustomFormat = "HH:mm:ss";
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTo.Location = new System.Drawing.Point(70, 90);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.ShowUpDown = true;
+            this.dtpTo.Size = new System.Drawing.Size(120, 20);
+            this.dtpTo.TabIndex = 24;
+            this.dtpTo.Value = new System.DateTime(2002, 2, 2, 0, 0, 0, 0);
+            this.dtpTo.ValueChanged += new System.EventHandler(this.dtpRegularWorkTo_ValueChanged);
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.CustomFormat = "HH:mm:ss";
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFrom.Location = new System.Drawing.Point(70, 54);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.ShowUpDown = true;
+            this.dtpFrom.Size = new System.Drawing.Size(120, 20);
+            this.dtpFrom.TabIndex = 23;
+            this.dtpFrom.Value = new System.DateTime(2002, 2, 2, 0, 0, 0, 0);
+            this.dtpFrom.ValueChanged += new System.EventHandler(this.dtpRegularWorkFrom_ValueChanged);
+            // 
+            // lblNextDay
+            // 
+            this.lblNextDay.AutoSize = true;
+            this.lblNextDay.Location = new System.Drawing.Point(196, 94);
+            this.lblNextDay.Name = "lblNextDay";
+            this.lblNextDay.Size = new System.Drawing.Size(53, 13);
+            this.lblNextDay.TabIndex = 22;
+            this.lblNextDay.Text = "[next day]";
+            this.lblNextDay.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "From";
+            // 
+            // frmAddUpdateShift
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 204);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.lblNextDay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblAddUpdateShift);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmAddUpdateShift";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Update Shift";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddUpdateShift;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Label lblNextDay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
